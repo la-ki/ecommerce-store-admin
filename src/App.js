f8765ae2b60c303f134e1003ac116b2c37eb16ee
la-ki <layarkostic94@gmail.com>
2020-10-13 14:22:57 +0200

    Added Category to Sidebar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {isUserLoggedIn} from './actions';
 import Products from './containers/Products';
 import Orders from './containers/Orders';
+import Category from './containers/Category';
 
 function App() {
 
@@ -25,8 +26,9 @@ function App() {
     <div className="App">
         <Switch>
           <PrivateRoute path="/" exact component={Home} />
-          <PrivateRoute path="/products" exact component={Products} />
-          <PrivateRoute path="/orders" exact component={Orders} />
+          <PrivateRoute path="/category" component={Category} />
+          <PrivateRoute path="/products" component={Products} />
+          <PrivateRoute path="/orders" component={Orders} />
 
           <Route path="/login" exact component={Login} />
           <Route path="/signup" exact component={Signup} />
