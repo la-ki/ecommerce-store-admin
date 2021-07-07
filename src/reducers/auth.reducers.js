124bc3b80ac52f227e0d228a1ef83956b7c97685
Lazar <65450353+la-ki@users.noreply.github.com>
2021-07-07 09:40:40 +0200

    Update auth.reducers.js

--- src/reducers/auth.reducers.js
+++ src/reducers/auth.reducers.js
@@ -17,8 +17,6 @@ const initState = {
 
 export default (state = initState, action) => {
 
-    console.log(action);
-
     switch (action.type) {
         case authConstants.LOGIN_REQUEST:
             state = {
